921cc3b9d5f93345f357c5768f6879bfda486195
nOOb3167 <nOOb3167@gmail.com>
2018-10-29 07:11:20 +0100

    ps: linking

--- src/stage2.cpp
+++ src/stage2.cpp
@@ -1,11 +1,14 @@
 #include <cstdlib>
 
+#include <boost/filesystem.hpp>
 #include <SFML/Window.hpp>
 
 int
 main(int argc, char **)
 {
 	sf::Window window(sf::VideoMode(800, 600), "perder.si");
+
+	boost::filesystem::current_path();
 
 	return EXIT_SUCCESS;
 }
