826d577aca23c6df947f6dd903bdbcaf5e1b6d7f
nOOb3167 <nOOb3167@gmail.com>
2018-11-02 05:50:48 +0100

    ps: fix gitignore hiding a config file

---
+++ files/config_server_default.py
@@ -0,0 +1,11 @@
+import os
+config = {
+        "LISTEN_HOST": "localhost.localdomain",
+        "LISTEN_PORT": "5201",
+        "ORIGIN_DOMAIN_APP": "localhost.localdomain",
+        "ORIGIN_DOMAIN_API": "api.localhost.localdomain",
+        "REPO_DIR": "./repo_s",
+        "RSYNC_CMD": "rsync"
+        "RSYNC_USER_HOST_PORT": ["", "localhost.localdomain", ""],
+        "TESTING": False,
+}
