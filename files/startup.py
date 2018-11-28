88c6a6f19119aecd9b73b05341b98aa491ebb333
nOOb3167 <nOOb3167@gmail.com>
2018-11-28 18:21:57 +0000

    ps: formatting and fixes

--- files/startup.py
+++ files/startup.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
+    import types
     from importlib import import_module as importlib__import_module
     from sys import argv as sys__argv
     mod: types.ModuleType = importlib__import_module(sys__argv[1])
