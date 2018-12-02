b506f375ddce8a407a660f9a06583e6689c33df6
nOOb3167 <nOOb3167@gmail.com>
2018-12-02 18:57:00 +0000

    ps: deployscript

--- files/config_deploy_script_andrej.sh
+++ files/config_deploy_script_andrej.sh
@@ -22,6 +22,7 @@ CMAKE_PARAMS="-DCMAKE_INSTALL_PREFIX=E:/prog/perdersi/RemPerdersiInst \
 PYTHON="/cygdrive/c/Users/Andrej/AppData/Local/Programs/Python/Python37-32/python"
 
 psexit() { echo "$*"; exit 1; }
+psdate_fromunix() { date --date=@"$1"; }
 
 case $1 in
     --build)
@@ -32,18 +33,38 @@ case $1 in
         $RSYNC -e "$SSH_" Andrej@localhost:/cygdrive/e/prog/perdersi/RemPerdersiInst/ /usr/local/perdersi/stage || psexit transfer remote local
         ;;
     --info-list)
-        echo dirs config marker
+        echo dirs config marker repo
         ;;
     --info)
         case $2 in
             dirs)
-                $SSH "[ -d '/cygdrive/e/prog/perdersi/RemPerdersiSrc' ] && echo Y || echo N ; [ -d '/cygdrive/e/prog/perdersi/RemPerdersiBuild' ] && echo Y || echo N ; [ -d '/cygdrive/e/prog/perdersi/RemPerdersiInst' ] && echo Y || echo N"
+                d0=/cygdrive/e/prog/perdersi/RemPerdersiSrc
+                d1=/cygdrive/e/prog/perdersi/RemPerdersiBuild
+                d2=/cygdrive/e/prog/perdersi/RemPerdersiInst
+                darray=("$d0" "$d1" "$d2")
+                tsarray=($($SSH "for d in '$d0' '$d1' '$d2'; do find \"\$d\" -printf '%T@\\n' | sort -g | tail -n 1; done"))
+                ynarray=($($SSH "for d in '$d0' '$d1' '$d2'; do [ -d \"\$d\" ] && echo Y || echo N; done"))
+                for d in 0 1 2; do
+                    echo -e ${darray[$d]} '\t' ${ynarray[$d]} '\t' $(psdate_fromunix "${tsarray[$d]}")
+                done
                 ;;
             config)
                 $SSH "PYTHONPATH='E:/prog/perdersi/RemPerdersiSrc/files;E:/prog/perdersi/RemPerdersiBuild' $PYTHON -c \"from pprint import pprint as p; import ps_config_deploy; p(ps_config_deploy.config); import ps_config_server; p(ps_config_server.config); import ps_config_updater; p(ps_config_updater.config)\""
                 ;;
             marker)
                 $SSH "cat E:/prog/perdersi/RemPerdersiBuild/ps_marker_remote | grep ON"
+                ;;
+            repo)
+                cd /usr/local/perdersi/repo_s
+                sha=$(git show-ref -s refs/heads/master)
+                com=$(git cat-file -p $sha)
+                comdate=$(git show -s --format="%ad" $sha)
+                echo == Latest Commit ==
+                echo $sha "  " refs/heads/master "  " $comdate
+                echo == Commit ==
+                IFS=
+                echo $com
+                unset IFS
                 ;;
             *) psexit args info ;;
         esac
