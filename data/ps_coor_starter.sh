5482daddacab7476cfb57cfc78830cba48534aee
nOOb3167 <nOOb3167@gmail.com>
2018-12-03 22:10:46 +0000

    ps: remove starter sh scripts

--- data/ps_coor_starter.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-VENPATH=/usr/local/perdersi/venv/bin
-WEBPATH=/usr/local/perdersi/deploy/web
-
-cd "$WEBPATH"
-PYTHONPATH="$WEBPATH" $VENPATH/python3 -m startup server_coor
