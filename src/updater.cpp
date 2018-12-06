cc7c15c514c37ef645e7ef148c3c973755403133
nOOb3167 <nOOb3167@gmail.com>
2018-12-06 04:50:49 +0100

    ps: config ARG_SKIPSELFUPDATE

--- src/updater.cpp
+++ src/updater.cpp
@@ -20,10 +20,9 @@ using namespace ps;
 class PsThr
 {
 public:
-	PsThr(const pt_t &config, bool arg_skipselfupdate, const sp<PsConTest> &client) :
+	PsThr(const pt_t &config, PsConTest *client) :
 		m_thr(),
 		m_config(config),
-		m_arg_skipselfupdate(arg_skipselfupdate),
 		m_client(client)
 	{}
 
@@ -56,7 +55,7 @@ public:
 		const boost::filesystem::path stage2path = chkoutdir / m_config.get<std::string>("UPDATER_STAGE2_EXE_RELATIVE");
 		const std::string updatr = m_config.get<std::string>("UPDATER_EXE_RELATIVE");
 
-		const shahex_t head = updater_head_get(m_client.get(), "master");
+		const shahex_t head = updater_head_get(m_client, "master");
 
 		std::cout << "repodir: " << git_repository_path(repo.get()) << std::endl;
 		std::cout << "chkodir: " << chkoutdir.string() << std::endl;
@@ -64,19 +63,18 @@ public:
 		std::cout << "updatr: " << updatr << std::endl;
 		std::cout << "head: " << head << std::endl;
 
-		const std::vector<shahex_t> trees = updater_trees_get_writing_recursive(m_client.get(), repo.get(), head);
+		const std::vector<shahex_t> trees = updater_trees_get_writing_recursive(m_client, repo.get(), head);
 		const std::vector<shahex_t> blobs = updater_blobs_list(repo.get(), trees);
-		updater_blobs_get_writing(m_client.get(), repo.get(), blobs);
+		updater_blobs_get_writing(m_client, repo.get(), blobs);
 		ns_git::checkout_obj(repo.get(), head, chkoutdir.string());
 
-		updater_replace_cond(m_arg_skipselfupdate, repo.get(), head, updatr, cruft_current_executable_filename(), stage2path);
+		updater_replace_cond(m_config.get<int>("ARG_SKIPSELFUPDATE"), repo.get(), head, updatr, cruft_current_executable_filename(), stage2path);
 	}
 
 	std::thread m_thr;
 	std::exception_ptr m_exc;
 	pt_t m_config;
-	bool m_arg_skipselfupdate;
-	sp<PsConTest> m_client;
+	PsConTest *m_client;
 };
 
 int main(int argc, char **argv)
@@ -89,9 +87,10 @@ int main(int argc, char **argv)
 		return 123;
 
 	pt_t config = cruft_config_read();
+	config.put("ARG_SKIPSELFUPDATE", (int)arg_skipselfupdate);
 
 	sp<PsConTest> client(new PsConTest(config.get<std::string>("ORIGIN_DOMAIN_API"), config.get<std::string>("LISTEN_PORT"), ""));
-	PsThr thr(config, arg_skipselfupdate, client);
+	PsThr thr(config, client.get());
 	thr.start();
 	thr.join();
 
