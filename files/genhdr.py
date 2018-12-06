6758603307617f79f0a2604bf3d9dcfe2a0835a2
nOOb3167 <nOOb3167@gmail.com>
2018-12-07 00:53:05 +0100

    ps: genhdr src_pth

--- files/genhdr.py
+++ files/genhdr.py
@@ -17,25 +17,38 @@ import types
 def err():
     raise RuntimeError();
 
-def make_hdr(json: str):
-    byt: bytes = json.encode("UTF-8")
+def make_hdr(byt: bytes):
     hex: str = byt.hex()
     return 'char g_ps_config[] = { ' + ''.join(['0x' + hex[2*i] + hex[2*i+1] + ", " for i in range(len(hex) // 2)]) + '};\n'
+
+def mod(src_mod: str):
+    # import src_mod and attempt to retrieve its config attribute as json
+    src_mod: types.ModuleType = importlib.import_module(src_mod)
+    src_config: dict = src_mod.config
+    src_config_json: str = json.dumps(src_config)
+    byt: bytes = src_config_json.encode("UTF-8")
+    return byt
+
+def pth(src_pth: str):
+    src: pathlib.Path = pathlib.Path(src_pth)
+    with src.open(mode='rb') as f:
+        return f.read()
 
 def run():
     # get args
     parser = argparse.ArgumentParser()
-    parser.add_argument('--src_mod', nargs=1, required=True)
+    parser.add_argument('--src_mod', nargs=1, required=False)
+    parser.add_argument('--src_pth', nargs=1, required=False)
     parser.add_argument('--dst', nargs=1, required=True, type=lambda x: x if x.endswith(".h") else err())
     args = parser.parse_args()
-    # import src_mod and attempt to retrieve its config attribute as json
-    src_mod: types.ModuleType = importlib.import_module(str(args.src_mod[0]))
-    src_config: dict = src_mod.config
-    src_config_json: str = json.dumps(src_config)
+    # either mod or pth
+    byt = mod(str(args.src_mod[0])) if args.src_mod is not None else \
+          pth(str(args.src_pth[0])) if args.src_pth is not None else \
+          err()
     # write dst
     dst: pathlib.Path = pathlib.Path(args.dst[0])
     with dst.open(mode='w', newline='\n') as f:
-        f.write(make_hdr(src_config_json))
+        f.write(make_hdr(byt))
 
 if __name__ == '__main__':
     run()
